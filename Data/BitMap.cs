9ae1e4abb6afd45f78851648a2c9b17480c059de
lanlichao <lanlichao@beisen.com>
2019-01-21 21:41:40 +0800

    1

---
+++ Data/BitMap.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class BitMap
+    {
+        private byte[] bytes;
+        private int nbits;
+        public BitMap(int nbits)
+        {
+            this.nbits = nbits;
+            var n = nbits / 8 + 1;
+            this.bytes = new byte[n];
+        }
+        public void set(int k)
+        {
+            if (k > nbits) return;
+            int byteIndex = k / 8;
+            int bitIndex = k % 8;
+            bytes[byteIndex] |=(byte) (1<<bitIndex);
+        }
+        public bool get(int k)
+        {
+            if (k > nbits) return false;
+            int byteIndex = k / 8;
+            int bitIndex = k % 8;
+            return (bytes[byteIndex] & (1 << bitIndex)) != 0;
+        }
+    }
+}
---
+++ Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 0,1,8,10,11,88,99,100,111,999};
+            foreach (var item in list)
+            {
+                BitMap b = new BitMap(item);
+                b.set(item);
+                var a = b.get(item);
+                var c = b.get(9);
+            }
+         
+        }
+    }
+}
