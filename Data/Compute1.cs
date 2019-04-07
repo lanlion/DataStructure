302cb9f066d486e9c0d7a4746a56df574baeb9cd
lanlichao <lanlichao@beisen.com>
2019-04-07 09:33:04 +0800

    计算

---
+++ Data/Compute1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    /// <summary>
+    /// 四则运算规则
+    /// 1.先乘除，后加减
+    /// 2.从左算到右
+    /// 3.先括号内，后括号外
+    /// </summary>
+    public class Compute1
+    {
+
+    }
+}
